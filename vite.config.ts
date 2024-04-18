38e7013445a65d63b0c48f488534d9ad90eb60e3
Sayan Biswas <me@sayanbiswas.in>
2024-04-18 18:01:53 +0530

    Add vite-plugin-pwa to package.json and vite.config.ts
    Signed-off-by: Sayan Biswas <me@sayanbiswas.in>

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { TanStackRouterVite } from '@tanstack/router-vite-plugin'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), TanStackRouterVite()],
+  plugins: [
+    react(),
+    TanStackRouterVite(),
+    VitePWA(
+      {
+        registerType: 'autoUpdate',
+        injectRegister: 'auto',
+        manifest: { theme_color: '#1c1b1e' }
+      })
+  ],
 })
